b45f0ac5c410e2f9afebbd4e779153cfb23886f2
elazar cohen <escohen115@gmail.com>
2020-04-07 20:18:35 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  counter = 0
+  board.each do |square|
+    if square == "X" || square == "O"
+      counter += 1
+    end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else 
+    return "O"
+  end
+end
